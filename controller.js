f98f1d0c327487e5b9fca2d909f5715d49e29940
Simões <mariaeduardasimoesal@gmail.com>
2023-08-08 08:36:20 -0300

    Finalização do codigo

--- controller.js
+++ controller.js
@@ -2,41 +2,50 @@ function acessar() {
   let loginEmail = document.getElementById("loginEmail").value;
   let loginSenha = document.getElementById("loginSenha").value;
 
-  if(!loginEmail || !loginSenha) {
-      alert("favor preencha os campos para acessar o site");
+  if (!loginEmail || !loginSenha) {
+    alert("favor preencha os campos para acessar o site");
   } else {
-     window.location.href = "aula2.html";
+    window.location.href = "fornecedores.html";
   }
 }
 // //FUNÇÃO PARA AMENIZAR NOMES EM ARRAY
 var dadosLista = [];
+var dadosCNPJ = [];
 
-function salvarUser(){
+function salvarUser() {
   let nomeUser = document.getElementById("nomeUser").value;
+  let nomeCNPJ = parseInt(document.getElementById("nomeCNPJ").value);
+  if (nomeUser && nomeCNPJ) {
+    dadosLista.push(nomeUser);
+    dadosCNPJ.push(nomeCNPJ);
+  }else{
+    alert("Dados de campos obrigatórios");
+  }
+  criarLista();
+  document.getElementById('nomeUser').value = '';
+  document.getElementById('nomeCNPJ').value = '';
 
-
-  if(nomeUser){
-      dadosLista.push (nomeUser);
-      criarLista();
-      document.getElementById('nomeUser').value = '';
 }
-}
+// //FUNÇÃO PARA AMENIZAR CNPJ EM ARRAY
 // FUNÇÃO PARA CRIAR LISTA DE USUÁRIO
-function criarLista(){
-  let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
-
-  for(let i = 0;i <=(dadosLista.length - 1);i++){
-      tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
-      document.getElementById('tabela').innerHTML = tabela;
+function criarLista() {
+  let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>CNPJ</th><th>Ações</th></tr>";
+  for (let i = 0; i <= (dadosLista.length - 1); i++) {
+    tabela += "<tr><td>" + dadosLista[i] + "</td><td>" + dadosCNPJ[i] + "</td><td><button class='btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+    document.getElementById('tabela').innerHTML = tabela;
   }
 }
-// FUNÇÃO EDITAR NOME 
-function editar(i){
+// FUNÇÃO EDITAR NOME e CNPJ
+function editar(i) {
   document.getElementById('nomeUser').value = dadosLista[(i - 1)];
-  dadosLista.splice(dadosLista[(i-1)], 1);
+  document.getElementById('nomeCNPJ').value = dadosCNPJ[(i - 1)];
+  dadosLista.splice(dadosLista[(i - 1)], 1);
+  dadosCNPJ.splice(dadosCNPJ[(i - 1)], 1);
 }
-// FUNÇÃO EXCLUIR NOME 
-function excluir(i){
-  dadosLista.splice((i-1),1);
+// FUNÇÃO EXCLUIR NOME e CNPJ
+function excluir(i) {
+  dadosLista.splice((i - 1), 1);
+  dadosCNPJ.splice((i - 1), 1);
+  document.getElementById('tabela').deleteRow(i);
   document.getElementById('tabela').deleteRow(i);
 }
